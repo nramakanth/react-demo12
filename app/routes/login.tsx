977e40d8b22c7bab9ba365efbd2d9d2a41c9cbff
Ramakanth <ramakanth.n@conquerorstech.net>
2025-07-21 18:13:06 +0530

    df

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -14,7 +14,7 @@ export default function Login() {
     e.preventDefault();
     setMessage("");
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://react-demo12-au82-688ix8dun-ramakanthn-conquerorstes-projects.vercel.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
